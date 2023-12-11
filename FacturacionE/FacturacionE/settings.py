30b9f92540ea6354f9a40968ff731e88231d2bcb
Orlando <orlando.hurtado@milwaukeetool.com>
2023-12-11 09:37:55 -0500

    Allowed hosts added

--- FacturacionE/FacturacionE/settings.py
+++ FacturacionE/FacturacionE/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-skr@=vl(z%u&66f&toojz+-0zb-9(+-%9w^#n+6b0fbgo-)vje'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alejanessan.pythonanywhere.com']
 
 
 # Application definition
